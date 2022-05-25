78617dcfb567ac7bbc32dce4e2443f30331b02d3
saurabh <saurabhkanawade30@gmail.com>
2022-05-25 09:46:12 +0530

    added the prime no program

---
+++ PrimeNo.java
@@ -0,0 +1,22 @@
+public class PrimeNo {
+
+    public static void main(String[] args) {
+
+        int no = 6;
+        int count = 1;
+
+        for (int i = 1; i < no; i++) {
+
+            if (no % i == 0) {
+
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.println("no is Prime");
+        } else {
+            System.out.println(" no is not prime");
+        }
+
+    }
+}
